10d983b9223bd4278ecf51d1ca80eb3feef17576
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-12-30 23:12:36 +0530

    Changed Slider Images

--- src/main/webapp/slider.js
+++ src/main/webapp/slider.js
@@ -16,56 +16,20 @@ const slides = [
     {
         id: 0,
         title: "Slide 1",
-        img: "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/1"
+        img: "https://i.postimg.cc/mrpH3X2T/Slider1.png",
+        link: "/categories/vegetables.jsp"
     },
     {
         id: 1,
         title: "Slide 2",
-        img: "https://images.unsplash.com/photo-1520763185298-1b434c919102?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/2"
+        img: "https://i.postimg.cc/Y9JLZvWV/Slider2.png",
+        link: "/categories/flours.jsp"
     },
     {
         id: 2,
         title: "Slide 3",
-        img: "https://images.unsplash.com/photo-1431263154979-0982327fccbb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/3"
-    },
-    {
-        id: 3,
-        title: "Slide 4",
-        img: "https://images.unsplash.com/photo-1496483648148-47c686dc86a8?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/4"
-    },
-    {
-        id: 4,
-        title: "Slide 5",
-        img: "https://images.unsplash.com/photo-1462524500090-89443873e2b4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/5"
-    },
-    {
-        id: 5,
-        title: "Slide 6",
-        img: "https://images.unsplash.com/photo-1588477000993-460d2257f88a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/6"
-    },
-    {
-        id: 6,
-        title: "Slide 7",
-        img: "https://images.unsplash.com/photo-1524303676975-5989d34c6854?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/7"
-    },
-    {
-        id: 7,
-        title: "Slide 8",
-        img: "https://images.unsplash.com/photo-1538340794916-c941f42976f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/8"
-    },
-    {
-        id: 8,
-        title: "Slide 9",
-        img: "https://images.unsplash.com/photo-1541878578057-63a626f9ed7b?q=80&w=2099&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        link: "https://www.example.com/9"
+        img: "https://i.postimg.cc/qB33nGfd/Slider3.png",
+        link: "/deals.jsp"
     }
 ];
 
